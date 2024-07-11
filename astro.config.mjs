ac36b31219654451ce34575a40377700b89c7ed8
ljn <ljnchn@qq.com>
2024-07-11 14:31:30 +0800

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,18 @@ import UnoCSS from '@unocss/astro'
 export default defineConfig({
     integrations: [
         UnoCSS({
-            injectReset: true // or a path to the reset file
+            injectReset: true, // or a path to the reset file
+            theme: {
+                extend: {
+                  colors: {
+                    gray: {
+                      900: '#1a1a1a',
+                    },
+                  },
+                },
+              },
+              // 开启暗模式
+              darkMode: 'class', // 或者 'media'
           }),
       ],
 });
